cfe7a6d86ab08d554a008a278de7a2f835784135
nas <nas.shihab92@gmail.com>
2018-04-01 22:26:07 +0600

    Initial commit

---
+++ app/src/main/java/com/example/nassh/fragment/CustomAdapter.java
@@ -0,0 +1,11 @@
+package com.example.nassh.fragment;
+
+import android.content.Context;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.PagerAdapter;
+
+import java.nio.charset.Charset;
+
